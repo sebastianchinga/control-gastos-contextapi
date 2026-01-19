e6b6551b410d8e7320879d1122c0a2109e51142e
Sebastian <jchingapalacios05@gmail.com>
2026-01-19 14:18:34 -0500

    Mostrando de forma condicional un componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useMemo } from "react";
 import BudgetForm from "./components/BudgetForm"
+import { useBudget } from "./hooks/useBudget"
+import BudgetTracker from "./components/BudgetTracker";
 
 function App() {
+
+  const { state } = useBudget();
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
 
   return (
     <>
@@ -9,7 +16,7 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTracker/> : <BudgetForm />}
       </div>
     </>
   )
