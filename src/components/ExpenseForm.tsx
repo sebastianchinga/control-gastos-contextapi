bf623184432803886b1e0a552a1e07acd4d2b76e
Sebastian <jchingapalacios05@gmail.com>
2026-01-21 00:16:49 -0500

    Agregando calendario al formulario

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,11 @@
 import { categories } from "../data/categories";
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function ExpenseForm() {
     return (
@@ -41,6 +48,11 @@ export default function ExpenseForm() {
                         <option value={category.id} key={category.id}>{category.name}</option>
                     ))}
                 </select>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <label htmlFor="amount" className="text-xl">Fecha Gasto:</label>
+                <DatePicker className='bg-slate-100 p-2 border-0' />
             </div>
 
             <input
