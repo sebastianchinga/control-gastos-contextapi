e43481ec8d9e6742bebb07e24338a107dc4046ff
Sebastian <jchingapalacios05@gmail.com>
2026-01-21 23:59:26 -0500

    Mostrando gastos

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,6 @@
+import { formarDate } from "../helpers"
 import type { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailProps = {
     expense: Expense
@@ -6,6 +8,17 @@ type ExpenseDetailProps = {
 
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
     return (
-        <div>ExpenseDetail</div>
+        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+            <div>
+
+            </div>
+            <div>
+                <p>{expense.expenseName}</p>
+                {/* El ! significa que esta garantizando que el valor existe */}
+                <p className="text-slate-600 text-sm">{formarDate(expense.date!.toString())}</p>
+            </div>
+
+            <AmountDisplay amount={expense.amount}/>
+        </div>
     )
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,18 @@ export function formatCurrency(amount: number): string {
         currency: 'USD'
     }).format(amount)
 }
+
+export function formarDate(date:string): string {
+    // Creamos un objeto date
+    const dateObj = new Date(date);
+    // Creamos las opciones
+    const options: Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    // Retornamos un string
+    return new Intl.DateTimeFormat('es-ES', options).format(dateObj);
+
+}
