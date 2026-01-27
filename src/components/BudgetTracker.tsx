eb8b77a87f21ccdad49fa26332fa504ed2f26b17
Sebastian <jchingapalacios05@gmail.com>
2026-01-27 18:32:31 -0500

    fix: Colocando funciones en el context

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -4,10 +4,9 @@ import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
 
-  const { state } = useBudget();
+  const { totalExpenses, remainingBudget, state } = useBudget();
 
-  const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => expense.amount + total, 0 ), [state.expenses])
-  const remainingBudget = state.budget - totalExpenses
+  
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,10 +1,12 @@
-import { createContext, useReducer, type ReactNode } from "react"
+import { createContext, useMemo, useReducer, type ReactNode } from "react"
 import { budgetReducer, initialState, type BudgetActions, type BudgetState } from "../reducers/budget-reducer"
 
 // Tipado para la creacion del context
 type BudgetContextProps = {
     state: BudgetState
     dispatch: React.ActionDispatch<[action: BudgetActions]>
+    totalExpenses: number
+    remainingBudget: number
 }
 
 // Tipado para el provider
@@ -16,16 +18,22 @@ type BudgetProviderProps = {
 export const BudgetContext = createContext<BudgetContextProps>(null!);
 
 // Creamos el provider
-export const BudgetProvider = ({children}: BudgetProviderProps) => {
+export const BudgetProvider = ({ children }: BudgetProviderProps) => {
 
     // Instanciamos el state y dispatch del useReducer
     const [state, dispatch] = useReducer(budgetReducer, initialState);
+
+    const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => expense.amount + total, 0), [state.expenses])
+
+    const remainingBudget = state.budget - totalExpenses
 
     // Retornamos el context creado y le pasamos sus valores que esperaba en BudgetContextProps: state y dispatch
     return (
         <BudgetContext.Provider value={{
             state,
-            dispatch
+            dispatch,
+            totalExpenses,
+            remainingBudget
         }}>
             {children}
         </BudgetContext.Provider>
