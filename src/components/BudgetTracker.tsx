14c37b2f13e01f1d82e036afda4e55ebf03c26dd
Sebastian <jchingapalacios05@gmail.com>
2026-02-01 15:46:35 -0500

    feat: add circular graph

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,14 +1,28 @@
+import {CircularProgressbar, buildStyles} from 'react-circular-progressbar'
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css"; // Imprtando hoja de estilos
 
 export default function BudgetTracker() {
 
   const { totalExpenses, remainingBudget, state } = useBudget();  
 
+  // toFixed: La cantidad de decimales
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2)
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="Gráfica de gastos" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage === 100 ? '#dc2626' : '#3B82F6',
+            trailColor: '#F5F5F5',
+            textSize: 8,
+            textColor: '#3B82F6'
+          })}
+          text={`${percentage}% Gastado`}
+        />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-8">
