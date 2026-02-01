c887fd1251497e0f9c383524646582b61e4c1d39
Sebastian <jchingapalacios05@gmail.com>
2026-02-01 16:23:47 -0500

    feat: show filtered expenses

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -3,16 +3,23 @@ import { useBudget } from "../hooks/useBudget"
 import ExpenseDetail from "./ExpenseDetail";
 
 export default function ExpenseList() {
+
     // Extraemos el state del hook
     const { state } = useBudget();
+
+    const filteredExpenses = state.currentCategory ? state.expenses.filter(expense => expense.category === state.currentCategory) : state.expenses
+
     // Validar si expenses es igual a 0 cada vez que state.expenses cambie
-    const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
+    const isEmpty = useMemo(() => filteredExpenses.length === 0, [filteredExpenses]);
+
+    
+
     return (
         <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
             {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay Gastos</p> : (
                 <>
                     <p className="text-gray-600 text-2xl font-bold my-5">Listado de Gastos.</p>
-                    {state.expenses.map(expense => (
+                    {filteredExpenses.map(expense => (
                         <ExpenseDetail key={expense.id} expense={expense}/>
                     ))}
                 </>
